7b6582a4f4ba05f0df4ece9a63f0637016f287d8
PushpakB3096 <rtpushpak@gmail.com>
2021-05-12 22:46:51 +0530

    Add list component

---
+++ src/components/Main/List/List.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import useStyles from "./styles";
+
+const List = () => {
+  return <div>List</div>;
+};
+
+export default List;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,6 +10,7 @@ import {
 import useStyles from "./styles";
 
 import Form from "./Form/Form";
+import List from "./List/List";
 
 const Main = () => {
   const classes = useStyles();
@@ -39,7 +40,8 @@ const Main = () => {
       <CardContent className={classes.cartContent}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            {/* TODO: List will be here */}
+            {/* List of existing entries */}
+            <List />
           </Grid>
         </Grid>
       </CardContent>
