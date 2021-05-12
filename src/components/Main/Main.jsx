9b33450f4aa851d26a6375870102a6e496bbafe6
PushpakB3096 <rtpushpak@gmail.com>
2021-05-12 18:17:42 +0530

    Add all three panes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -12,10 +12,33 @@ const Main = () => {
   const classes = useStyles();
   return (
     <Card className={classes.root}>
+      {/* header for main card goes here */}
       <CardHeader
         title="Voice Powered Expense Tracker"
         subheader="By Pushpak Bhattacharya"
       />
+      <CardContent>
+        {/* displays total balance here */}
+        <Typography align="center" variant="h6">
+          Total Balance: ₹20400
+        </Typography>
+        {/* displays info on how to use it for the user */}
+        <Typography
+          variant="subtitle2"
+          style={{ lineHeight: "1.5em", marginTop: "20px" }}
+        >
+          Try saying: Add income for ₹100 in salary category for Friday...
+        </Typography>
+        <Divider />
+        {/* TODO: form will be here */}
+      </CardContent>
+      <CardContent className={classes.cartContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            {/* TODO: List will be here */}
+          </Grid>
+        </Grid>
+      </CardContent>
     </Card>
   );
 };
