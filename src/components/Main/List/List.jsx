a8df2db57c049811324000db7e17c89697b786db
PushpakB3096 <rtpushpak@gmail.com>
2021-05-12 23:02:39 +0530

    Add list transaction section

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,8 +1,65 @@
 import React from "react";
+import {
+  List as MUIList, // importing under a different name because our component is also named the same
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Avatar,
+  ListItemSecondaryAction,
+  IconButton,
+  Slide,
+} from "@material-ui/core";
+import { Delete, MoneyOff } from "@material-ui/icons";
+
 import useStyles from "./styles";
 
 const List = () => {
-  return <div>List</div>;
+  const classes = useStyles();
+
+  // dummy transactions
+  const transactions = [
+    { id: 1, type: "Income", category: "Salary", amount: 50, date: new Date() },
+  ];
+
+  return (
+    <MUIList dense={false} className={classes.list}>
+      {transactions.map((transaction) => (
+        // anything inside Slide component has an animation of sliding in/out
+        <Slide
+          direction="down"
+          in
+          mountOnEnter
+          unmountOnExit
+          key={transaction.id}
+        >
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar
+                // adding classes based on transaction type
+                className={
+                  transaction.type === "Income"
+                    ? classes.avatarIncome
+                    : classes.avatarExpense
+                }
+              >
+                <MoneyOff />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary={transaction.category}
+              secondary={`₹${transaction.amount} - ${transaction.date}`}
+            />
+            {/* delete transaction button */}
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="delete" onClick="">
+                <Delete />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </Slide>
+      ))}
+    </MUIList>
+  );
 };
 
 export default List;
