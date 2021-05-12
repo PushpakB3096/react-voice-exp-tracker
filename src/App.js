dd3876b42d16593e986b64b7d337edf88016e235
PushpakB3096 <rtpushpak@gmail.com>
2021-05-12 17:48:20 +0530

    Add styles for details pane

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Details from "./components/Details/Details";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Details />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Card, CardHeader, CardContent, Typography } from "@material-ui/core";
 import { Doughnut } from "react-chartjs-2";
 
+import useStyles from "./styles";
+
 const Details = () => {
+  const classes = useStyles();
+
   return (
     <Card>
       <CardHeader title="Income" />
