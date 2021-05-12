0cbb21ff3699f23abc6ada7e94fcef2a8c302b46
PushpakB3096 <rtpushpak@gmail.com>
2021-05-12 22:04:46 +0530

    Add form component

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import {
+  TextField,
+  Typography,
+  Grid,
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
 
 const Form = () => {
   return <div>Form</div>;
 };
+
+export default Form;
