e02a022d7efd0ac611c11dcf23a4184284e02b8e
PushpakB3096 <rtpushpak@gmail.com>
2021-05-13 10:19:16 +0530

    Add action creators and reducer for transactions

--- src/context/context.js
+++ src/context/context.js
@@ -1,4 +1,5 @@
 import React, { useReducer, createContext } from "react";
+import contextReducer from "./contextReducer";
 
 // initially we will have no transactions
 const initialState = [];
@@ -6,8 +7,31 @@ const initialState = [];
 export const ExpenseTrackerContext = createContext(initialState);
 
 export const Provider = ({ children }) => {
+  // use of reducer for adding or deleting transactions
+  const [transactions, dispatch] = useReducer(contextReducer, initialState);
+
+  // action creator to delete a transaction
+  const deleteTransaction = (id) => {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  };
+  // action creator to add a transaction
+  const addTransaction = (transaction) => {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  };
+
   return (
-    <ExpenseTrackerContext.Provider value={{ appName: "Expense Tracker" }}>
+    <ExpenseTrackerContext.Provider
+      value={{
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
       {children}
     </ExpenseTrackerContext.Provider>
   );
---
+++ src/context/contextReducer.js
@@ -0,0 +1,3 @@
+const contextReducer = () => {};
+
+export default contextReducer;
