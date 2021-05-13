3ec008b0e37adde9ce0434e340ec0065a531caf6
PushpakB3096 <rtpushpak@gmail.com>
2021-05-13 16:58:04 +0530

    Add custom toast msg (snackbar)

---
+++ src/components/Snackbar/Snackbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab";
+
+import useStyles from "./styles";
+
+const CustomSnackbar = () => {
+  const classes = useStyles();
+
+  return <div>Snackbar</div>;
+};
